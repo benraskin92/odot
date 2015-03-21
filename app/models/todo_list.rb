da037e7d7af7a5e9b14d012a9c313ad1679c3ff4
Benjamin <benraskin92@gmail.com>
2015-03-20 23:34:01 -0400

    Test2

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,5 @@
 class TodoList < ActiveRecord::Base
-	#validates :title, prescence: true
+	lidates :title, prescence: true
 	validates :title, length: {minimum: 3}
 	validates :description, length: {minimum: 5}
 	validates :description, prescence: true
