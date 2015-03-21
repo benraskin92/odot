dec5508bda7969acc34747d5dcc79cdb89af94d3
Benjamin <benraskin92@gmail.com>
2015-03-20 23:04:35 -0400

    Validation

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,6 @@
 class TodoList < ActiveRecord::Base
 	validates :title, prescence: true
 	validates :title, length: {minimum: 3}
+	validates :description, length: {minimum: 5}
+	validates :description, prescence: true
 end
